c0de2924f1eb61603655da5403c28791df01d80b
Luca Valnegri <l.valnegri@datamaps.co.uk>
2023-01-08 20:03:52 +0000

    added better names from HoCLib for MSOA (and LSOA)

--- data-raw/06-download_census_hierarchy.R
+++ data-raw/06-download_census_hierarchy.R
@@ -13,19 +13,21 @@ qs::qsave(y, './data-raw/qs/OA.bgc')
 y <- y |> rmapshaper::ms_simplify(0.5, keep_shapes = TRUE, sys = TRUE, sys_mem = 32) |> sf::st_transform(4326) |> sf::st_make_valid()
 qs::qsave(y, './data-raw/qs/OA')
 
-# LSOA & MSOA boundaries
-y <- Rgeo::dwn_shp_zip(paste0(ons_url['pfx'], 'bd30d8dcb84a4c32a763fc2d916a52bb', ons_url['sfx']), 'LSOA21CD', 'LSOA', out_path = './data-raw/shp/')
-y <- Rgeo::dwn_shp_zip(paste0(ons_url['pfx'], 'e45be904852d428d963abb33ed6ddbea', ons_url['sfx']), 'MSOA21CD', 'MSOA', out_path = './data-raw/shp/')
-
+# lookups & locations
 yo <- fread('https://www.arcgis.com/sharing/rest/content/items/792f7ab3a99d403ca02cc9ca1cf8af02/data')
 yu <- fread('https://opendata.arcgis.com/api/v3/datasets/7207b51700f7472e88460f3a2e1eb5f9_0/downloads/data?format=csv&spatialRefId=4326&where=1%3D1')
-
-# lookups & locations
+yl <- unique(yo[, .(id = lsoa21cd, name = lsoa21nm)])[unique(lkp[, .(LSOA, parent = MSOA)]), on = c(id = 'LSOA')]
+ym <- fread(
+    'https://houseofcommonslibrary.github.io/msoanames/MSOA-Names-Latest2.csv', 
+    select = c('msoa21cd', 'msoa21hclnm'), 
+    col.names = c('MSOA', 'MSOAn')
+)
+ym <- ym[yl, on = c(MSOA = 'parent')][, LSOAn := paste(MSOAn, substring(name, nchar(name)))]
 lkp <- yo[, .(OA = oa21cd, LSOA = lsoa21cd, MSOA = msoa21cd)][yu[, .(OA = OA21CD, LTLA = LTLA22CD, UTLA = UTLA22CD, RGN = RGN22CD, CTRY = CTRY22CD)], on = 'OA']
 lca <- rbindlist(list(
     yo[, .(type = 'OA', id = oa21cd, name = oa21cd)][lkp[, .(OA, parent = LSOA, country = substr(OA, 1, 1))], on = c(id = 'OA')],
-    unique(yo[, .('LSOA', id = lsoa21cd, lsoa21nm)])[unique(lkp[, .(LSOA, MSOA, substr(LSOA, 1, 1))]), on = c(id = 'LSOA')],
-    unique(yo[, .('MSOA', id = msoa21cd, msoa21nm)])[unique(lkp[, .(MSOA, LTLA, substr(MSOA, 1, 1))]), on = c(id = 'MSOA')],
+    ym[, .('LSOA', id, LSOAn, MSOA, substr(id, 1, 1))],
+    unique(ym[, .('MSOA', id = MSOA, MSOAn)])[unique(lkp[, .(MSOA, LTLA, substr(MSOA, 1, 1))]), on = c(id = 'MSOA')],
     unique(yu[, .('LTLA', id = LTLA22CD, LTLA22NM)])[unique(lkp[, .(LTLA, UTLA, substr(LTLA, 1, 1))]), on = c(id = 'LTLA')],
     unique(yu[, .('UTLA', id = UTLA22CD, UTLA22NM)])[unique(lkp[, .(UTLA, RGN, substr(UTLA, 1, 1))]), on = c(id = 'UTLA')],
     unique(yu[, .('RGN', id = RGN22CD, RGN22NM)])[unique(lkp[, .(RGN, CTRY, substr(RGN, 1, 1))]), on = c(id = 'RGN')],
