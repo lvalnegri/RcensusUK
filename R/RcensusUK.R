c8680d0f9f79c26f828455eab74b1e66c3894f3d
Luca Valnegri <l.valnegri@datamaps.co.uk>
2023-01-20 19:15:35 +0000

    added description

--- R/RcensusUK.R
+++ R/RcensusUK.R
@@ -2,6 +2,15 @@
 #' 
 #' Data and functionalities for querying and mapping related to the UK Census 2021.
 #'  
+#' As of Jan-23, data at *small area* levels are available only for England and Wales
+#' 
+#' See the `README` file for more details.
+#' 
+#' Attributions:
+#' - Contains OS data © Crown copyright and database rights 2023 
+#' - Source: Office for National Statistics licensed under the [Open Government Licence v.3.0](http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)
+#' - Contains Parliamentary information licensed under the [Open Parliament Licence v3.0](https://www.parliament.uk/site-information/copyright/open-parliament-licence/)
+#'  
 #' @docType package
 #' @name RcensusUK
 NULL
