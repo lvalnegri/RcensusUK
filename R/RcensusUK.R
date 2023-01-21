31deeb6d3d43938f39d711c2bd7a0bf68eaa17c6
Luca Valnegri <l.valnegri@datamaps.co.uk>
2023-01-21 01:34:51 +0000

    added Meta Attribution

--- R/RcensusUK.R
+++ R/RcensusUK.R
@@ -10,6 +10,7 @@
 #' - Contains OS data © Crown copyright and database rights 2023 
 #' - Source: Office for National Statistics licensed under the [Open Government Licence v.3.0](http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)
 #' - Contains Parliamentary information licensed under the [Open Parliament Licence v3.0](https://www.parliament.uk/site-information/copyright/open-parliament-licence/)
+#' - Facebook Connectivity Lab and Center for International Earth Science Information Network - CIESIN - Columbia University. 2016. High Resolution Settlement Layer (HRSL). Source imagery for HRSL © 2016 DigitalGlobe. Accessed 15 Dec 2022.
 #'  
 #' @docType package
 #' @name RcensusUK
